95b19f433ac8767461134c5cc89b1c74f116d65d
Satont <satontworldwide@gmail.com>
2021-06-27 14:06:56 +0300

    Add analystics

--- run.py
+++ run.py
@@ -2,4 +2,5 @@ import os
 
 from TwitchChannelPointsMiner import TwitchChannelPointsMiner
 twitch_miner = TwitchChannelPointsMiner(os.getenv('USER'), os.getenv('PASSWORD'))
+twitch_miner.analytics(host="127.0.0.1", port=3000, refresh=5) 
 twitch_miner.mine(followers=True)
